7f91135cf543d079798aa6a35734173ab41790ba
Youen Péron <youen.peron@gmail.com>
2024-10-10 13:15:18 +0000

    feat : combination

--- src/main.v
+++ src/main.v
@@ -1,11 +1,81 @@
 module main
 
+import vsl.iter
+
 fn main() {
-	not_a_number := 'aa'
-	number := not_a_number.int()
-	println(not_a_number + number.str())	
+	s :="abc"
+	println(s)
+	mut allcombi := []string{}
+
+	ckin := new_choice_k_in(s, 2)
+	
+	for combi in ckin {
+		allcombi << combi
+	}
+
+	println(allcombi)
 }
 
-fn anagramme(seed string) []string {
-	return ["niche"]
+struct Anagramme {
+	index map[string][]string
+}
+
+fn new_anagramme() Anagramme {
+	return Anagramme{
+		index : {
+			"cehin" : ["chien", "niche"]
+			"eegin" : ["genie", "neige"]
+		}
+	}
+}
+
+fn (a Anagramme) find( word string) []string {
+	return a.index[a.seed(word)]
+}
+
+fn (a Anagramme) seed( word string) string {
+	mut runes := word.runes()
+	runes.sort()
+	return runes.string()
+}
+
+struct SeedSplitter {
+	seed string
+mut:
+	k int = 2
+}
+
+fn (s SeedSplitter) next() ?[]string {
+	return none
+}
+
+struct ChoiceKIn {
+mut:
+	iter iter.CombinationsIter
+}
+
+fn new_choice_k_in(seed string, k int) ChoiceKIn {
+	mut code := []f64{}
+	for l in seed.bytes() {
+		code << f64(l)
+	}
+	return ChoiceKIn {
+		iter : iter.CombinationsIter.new(code, k)
+	} 
+}
+
+fn (mut c ChoiceKIn) next() ?string {
+	
+
+	combi := c.iter.next() or { 
+		return none
+		} 
+
+	mut combi_u8 := []u8{}
+	for n in combi {
+		combi_u8 << u8(n)
+	}
+
+	return combi_u8.bytestr()
+	
 }
--- src/main_test.v
+++ src/main_test.v
@@ -1,5 +1,44 @@
 module main
 
+
 fn test_anagrame () {
-	assert "niche" in anagramme("chien")
+	anagramme := new_anagramme()
+	
+	assert "niche" in anagramme.find("chien")
+	assert "neige" in anagramme.find("genie")
+	//assert "neige niche" in anagramme.find("chien genie")
+
+}
+
+fn test_seed_spliter() {
+	
+	ss := SeedSplitter{
+		seed : "eegin"
+	}
+
+	mut subseeds := [][]string{}
+	for subseed in ss {
+		subseeds << subseed
+	}
+
+	assert subseeds == [
+	]
+}
+
+fn test_choice_k_in() {
+	
+	s :="abc"
+	mut allcombi := []string{}
+
+	ckin := new_choice_k_in(s, 2)
+	
+	for combi in ckin {
+		allcombi << combi
+	}
+
+	assert allcombi == [
+		'ab',
+		'ac',
+		'bc',
+	]
 }
