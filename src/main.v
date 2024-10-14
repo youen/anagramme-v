3cea6fce6e9dce4aef0d6db39326bedad89f8fba
Youen Péron <youen.peron@gmail.com>
2024-10-14 19:15:49 +0000

    chore: format file

--- src/main.v
+++ src/main.v
@@ -7,25 +7,23 @@ import arrays
 
 fn main() {
 	mut anagramme := new_anagramme()
-	anagramme.load_file("data/francais.txt")!
-	anagramme.load_file("data/1grams_french.csv")!
-	anagramme.load_file("data/2grams_french.csv")!
-	anagramme.load_file("data/3grams_french.csv")!
-	anagramme.load_file("data/4grams_french.csv")!
-	anagramme.load_file("data/5grams_french.csv")!
+	anagramme.load_file('data/francais.txt')!
+	anagramme.load_file('data/1grams_french.csv')!
+	anagramme.load_file('data/2grams_french.csv')!
+	anagramme.load_file('data/3grams_french.csv')!
+	anagramme.load_file('data/4grams_french.csv')!
+	anagramme.load_file('data/5grams_french.csv')!
 
-
-//	println(anagramme.find("lino pimo")) // mini pool
-//	println(anagramme.find("adrien youen")) // 'denier noyau'
-//	println(anagramme.find("cgi n est pas a vendre")) // 'passant divergence'
-//	println(anagramme.find("eric simon")) // roi mince
-//	println(anagramme.find("france travail")) // 'flairer vacant' 
-//	println(anagramme.find("cgi n est pas a vendre"))
-	println(">>> ${os.args[1]}")
-	for result in  anagramme.find_iterator(os.args[1]) {
+	//	println(anagramme.find("lino pimo")) // mini pool
+	//	println(anagramme.find("adrien youen")) // 'denier noyau'
+	//	println(anagramme.find("cgi n est pas a vendre")) // 'passant divergence'
+	//	println(anagramme.find("eric simon")) // roi mince
+	//	println(anagramme.find("france travail")) // 'flairer vacant'
+	//	println(anagramme.find("cgi n est pas a vendre"))
+	println('>>> ${os.args[1]}')
+	for result in anagramme.find_iterator(os.args[1]) {
 		println(result)
 	}
-	
 }
 
 struct Anagramme {
@@ -35,13 +33,12 @@ mut:
 
 fn new_anagramme() Anagramme {
 	return Anagramme{
-		index : {
-
+		index: {
 		}
 	}
 }
 
-fn find_subseeds (seed string)  [][]string {
+fn find_subseeds(seed string) [][]string {
 	ss := new_seed_splitter(seed)
 
 	mut subseeds := [][]string{}
@@ -50,31 +47,25 @@ fn find_subseeds (seed string)  [][]string {
 	}
 
 	return subseeds
-
 }
 
-fn (mut a Anagramme) add( word string)  {
-
+fn (mut a Anagramme) add(word string) {
 	a.index[a.seed(word)] << word
-
 }
 
 fn (mut a Anagramme) load_file(filename string) ! {
-	data := os.read_file(filename) !
-    mut reader := csv.new_reader(data)
+	data := os.read_file(filename)!
+	mut reader := csv.new_reader(data)
 	// read header
-	reader.read() !
-    for {
-        word := reader.read() or {
-            break
-        }
-        a.add(word[0])
-    }
+	reader.read()!
+	for {
+		word := reader.read() or { break }
+		a.add(word[0])
+	}
 }
 
-fn (a Anagramme) find( word string) []string {
-
-	mut result :=  []string{}
+fn (a Anagramme) find(word string) []string {
+	mut result := []string{}
 	for anagramme in a.find_iterator(word) {
 		result << anagramme
 	}
@@ -89,28 +80,24 @@ enum AnagrammeIteratorState as u8 {
 	recursive
 }
 
-
 struct AnagrammeIterator {
 	anagramme Anagramme
-	seed string
-
+	seed      string
 mut:
-	ss SeedSplitter
-	state AnagrammeIteratorState
+	ss     SeedSplitter
+	state  AnagrammeIteratorState
 	buffer []string
-	cache map[string]bool
-
+	cache  map[string]bool
 }
 
-fn (a Anagramme) find_iterator( word string) AnagrammeIterator {
+fn (a Anagramme) find_iterator(word string) AnagrammeIterator {
 	seed := a.seed(word)
 	return AnagrammeIterator{
-		anagramme : a
-		seed : seed
-		ss : new_seed_splitter(seed)
-		state : AnagrammeIteratorState.start
+		anagramme: a
+		seed:      seed
+		ss:        new_seed_splitter(seed)
+		state:     AnagrammeIteratorState.start
 	}
-
 }
 
 fn (mut a AnagrammeIterator) next() ?string {
@@ -119,7 +106,6 @@ fn (mut a AnagrammeIterator) next() ?string {
 			.start {
 				a.buffer = a.anagramme.index[a.seed]
 				a.state = AnagrammeIteratorState.direct
-
 			}
 			.direct {
 				if a.buffer.len > 0 {
@@ -131,78 +117,69 @@ fn (mut a AnagrammeIterator) next() ?string {
 						a.cache[result] = true
 						return result
 					}
-
 				} else {
 					a.state = AnagrammeIteratorState.subseed
 				}
-
 			}
 			.subseed {
 				if a.buffer.len > 0 {
-		
 					return a.buffer.pop()
-					
 				} else {
-					subseed := a.ss.next() or {  
+					subseed := a.ss.next() or {
 						a.state = AnagrammeIteratorState.recursive
-						a.ss =  new_seed_splitter(a.seed)
+						a.ss = new_seed_splitter(a.seed)
 						continue
 					}
 
-					first_col := a.anagramme.index[subseed[0]] or {  continue }
-					second_col := a.anagramme.index[subseed[1]] or {  continue }
-					for first in first_col{
+					first_col := a.anagramme.index[subseed[0]] or { continue }
+					second_col := a.anagramme.index[subseed[1]] or { continue }
+					for first in first_col {
 						for second in second_col {
-							result := "${first} ${second}"
-							ratio := f64(result.count(" ")) / f64(result.len) 
-							//println("${result} : ${ratio}")
-							if (ratio) >  0.1 {
+							result := '${first} ${second}'
+							ratio := f64(result.count(' ')) / f64(result.len)
+							// println("${result} : ${ratio}")
+							if ratio > 0.1 {
 								continue
 							}
-							if !(result in a.cache ){
+							if result !in a.cache {
 								a.cache[result] = true
-								a.buffer <<  result
+								a.buffer << result
 							}
 						}
 					}
 				}
-
 			}
 			.recursive {
 				if a.buffer.len > 0 {
-		
 					return a.buffer.pop()
-					
 				} else {
-					subseed := a.ss.next() or { return none}
+					subseed := a.ss.next() or { return none }
 
-					first_col := a.anagramme.index[subseed[0]] or {  continue }
-					for first in first_col{
+					first_col := a.anagramme.index[subseed[0]] or { continue }
+					for first in first_col {
 						for second in a.anagramme.find_iterator(subseed[1]) {
-							result := "${first} ${second}"
-							ratio := f64(result.count(" ")) / f64(result.len) 
-							if (ratio) >  0.2 {
+							result := '${first} ${second}'
+							ratio := f64(result.count(' ')) / f64(result.len)
+							if ratio > 0.2 {
 								continue
 							}
 							// println("${result} : ${ratio}")
-							if !(result in a.cache ){
+							if result !in a.cache {
 								a.cache[result] = true
-								a.buffer <<  result
+								a.buffer << result
 							}
 						}
 					}
 				}
-
 			}
 		}
 	}
-	
-	return none
 
+	return none
 }
 
-fn (a Anagramme) seed( word string) string {
-	mut runes := word.replace(" ","").runes()
+fn (a Anagramme) seed(word string) string {
+	mut runes := word.replace(' ', '').runes()
 	runes.sort()
 	return runes.string()
 }
@@ -210,40 +187,36 @@ fn (a Anagramme) seed( word string) string {
 struct SeedSplitter {
 	seed string
 mut:
-	k int
+	k           int
 	choice_k_in ChoiceKIn
 }
 
 fn new_seed_splitter(seed string) SeedSplitter {
-	return SeedSplitter {
-		seed : seed,
-		k : 2
-		choice_k_in : new_choice_k_in(seed, 2)
+	return SeedSplitter{
+		seed:        seed
+		k:           2
+		choice_k_in: new_choice_k_in(seed, 2)
 	}
 }
 
 fn (mut s SeedSplitter) next() ?[]string {
-
-	combi := s.choice_k_in.next() or { 
+	combi := s.choice_k_in.next() or {
 		if s.k < s.seed.len {
 			s.k++
 			s.choice_k_in = new_choice_k_in(s.seed, s.k)
 			return s.next()
 		}
 		return none
-		}
+	}
 
-	mut first := ""
-	mut second := ""
-	for index in 0..s.seed.len {
+	mut first := ''
+	mut second := ''
+	for index in 0 .. s.seed.len {
 		if index in combi {
-			 first += s.seed[index].ascii_str()
+			first += s.seed[index].ascii_str()
 		} else {
-
-		second += s.seed[index].ascii_str()
+			second += s.seed[index].ascii_str()
 		}
-
-
 	}
 
 	return [first, second]
@@ -256,20 +229,16 @@ mut:
 
 fn new_choice_k_in(seed string, k int) ChoiceKIn {
 	mut code := []f64{}
-	for index in 0..seed.len {
+	for index in 0 .. seed.len {
 		code << f64(index)
 	}
-	return ChoiceKIn {
-		iter : iter.CombinationsIter.new(code, k)
-	} 
+	return ChoiceKIn{
+		iter: iter.CombinationsIter.new(code, k)
+	}
 }
 
 fn (mut c ChoiceKIn) next() ?[]int {
-	
-
-	combi := c.iter.next() or { 
-		return none
-		} 
+	combi := c.iter.next() or { return none }
 
 	mut combi_int := []int{}
 	for n in combi {
@@ -277,5 +246,4 @@ fn (mut c ChoiceKIn) next() ?[]int {
 	}
 
 	return combi_int
-	
 }
--- src/main_test.v
+++ src/main_test.v
@@ -1,58 +1,49 @@
 module main
 
-
-fn test_anagrame () {
+fn test_anagrame() {
 	mut anagramme := new_anagramme()
-	anagramme.add("chien")
-	anagramme.add("niche")
-	anagramme.add("genie")
-	anagramme.add("neige")
-	
-	assert "niche" in anagramme.find("chien")
-	assert "neige" in anagramme.find("genie")
+	anagramme.add('chien')
+	anagramme.add('niche')
+	anagramme.add('genie')
+	anagramme.add('neige')
 
-	assert "neige niche" in anagramme.find("chien genie")
-	assert  ['chien genie', 'chien neige', 'genie chien', 'genie niche', 'neige chien', 'neige niche', 'niche genie', 'niche neige'] == anagramme.find("chien genie")
+	assert 'niche' in anagramme.find('chien')
+	assert 'neige' in anagramme.find('genie')
 
-	// assert "neige niche genie" in anagramme.find("chien genie neige")
+	assert 'neige niche' in anagramme.find('chien genie')
+	assert ['chien genie', 'chien neige', 'genie chien', 'genie niche', 'neige chien', 'neige niche',
+		'niche genie', 'niche neige'] == anagramme.find('chien genie')
 
+	assert "neige niche genie" in anagramme.find("chien genie neige")
 }
 
-fn test_anagrame_iterator () {
+fn test_anagrame_iterator() {
 	mut anagramme := new_anagramme()
-	anagramme.add("chien")
-	anagramme.add("niche")
-	anagramme.add("genie")
-	anagramme.add("neige")
+	anagramme.add('chien')
+	anagramme.add('niche')
+	anagramme.add('genie')
+	anagramme.add('neige')
 
-	mut result := anagramme.find_iterator("chien")
-	assert result.next() or { "" } == "niche"
+	mut result := anagramme.find_iterator('chien')
+	assert result.next() or { '' } == 'niche'
 
-
-	result = anagramme.find_iterator("chien genie")
-	assert result.next() or { "" } == "niche neige"
-
+	result = anagramme.find_iterator('chien genie')
+	assert result.next() or { '' } == 'niche neige'
 }
-
-
-
 
 fn test_seed_spliter() {
-
-	assert ["ce", "hin"] in find_subseeds("cehin")
-	assert ["ee", "gin"] in find_subseeds("eegin")
-	assert ["eg", "ein"] in find_subseeds("eegin")
-	assert ["cehin", "eegin"] in find_subseeds("ceeeghiinn")
-
+	assert ['ce', 'hin'] in find_subseeds('cehin')
+	assert ['ee', 'gin'] in find_subseeds('eegin')
+	assert ['eg', 'ein'] in find_subseeds('eegin')
+	assert ['cehin', 'eegin'] in find_subseeds('ceeeghiinn')
 }
 
 fn test_choice_k_in() {
-	
-	s :="abc"
+	s := 'abc'
 	mut allcombi := [][]int{}
 
 	ckin := new_choice_k_in(s, 2)
-	
+
 	for combi in ckin {
 		allcombi << combi
 	}
